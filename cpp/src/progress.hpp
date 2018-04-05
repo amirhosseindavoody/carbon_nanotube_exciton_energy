358d43e7dd399b8b1796e9fc6e96731372311797
Amirhossein Davoody <amirhossein.davoody@gmail.com>
2018-04-05 14:17:43 -0500

    updated progress.hpp

--- cpp/src/progress.hpp
+++ cpp/src/progress.hpp
@@ -10,129 +10,140 @@
 class progress_bar
 {
 private:
-  const int _barWidth=100;
-  int _pos=0;
-  float _progress=0;
-  std::string _title="";
-  int _i_max=0;
-  int _i=0;
+  const int _barWidth = 100;
+  int _pos = 0;
+  float _progress = 0;
+  std::string _title = "";
+  int _i_max = 0;
+  int _i = 0;
 
-  bool _is_silent=false;
+  bool _is_silent = false;
 
   bool _start_chrono = true;
   std::time_t _start_time;
   std::time_t _current_time;
 
-
 public:
   // constructor to initialize internal state of the progress bar
-  progress_bar(const int& i_max, const std::string& title="", const bool& is_silent=false)
+  progress_bar(const int &i_max, const std::string &title = "", const bool &is_silent = false)
   {
     _title = title;
-    _i_max = i_max-1;
-    _i=0;
-    
+    _i_max = i_max - 1;
+    _i = 0;
+
     _is_silent = is_silent;
-    
-    if (not _is_silent){
-      std::cout << _title << ":\n";
+
+    if (not _is_silent)
+    {
+      std::cout << _title << ":" << std::endl;
     }
-    
   };
 
   std::string estimate_remaining_time()
   {
 
     std::string remaining_time;
-    if (_start_chrono){
+    if (_start_chrono)
+    {
       _start_time = std::time(nullptr);
       _current_time = std::time(nullptr);
       _start_chrono = false;
     }
-    else {
+    else
+    {
       _current_time = std::time(nullptr);
     }
 
-    int sec = int(std::difftime(_current_time,_start_time)*(1-_progress)/(_progress+0.001));
+    int sec = int(std::difftime(_current_time, _start_time) * (1 - _progress) / (_progress + 0.001));
 
-    int hour = sec/3600;
-	  sec = sec%3600;
-	  int min = sec/60;
-	  sec = sec%60;
-	  std::stringstream ss;
-    ss << "remaining time " << std::setw(2) << std::setfill('0') << hour << ":" 
-                            << std::setw(2) << std::setfill('0') << min << ":" 
-                            << std::setw(2) << std::setfill('0') << sec;
+    int hour = sec / 3600;
+    sec = sec % 3600;
+    int min = sec / 60;
+    sec = sec % 60;
+    std::stringstream ss;
+    ss << "remaining time " << std::setw(2) << std::setfill('0') << hour << ":"
+       << std::setw(2) << std::setfill('0') << min << ":"
+       << std::setw(2) << std::setfill('0') << sec;
     return ss.str();
   };
-
-
 
   // stepping function to keep track of progress internally
   void step()
   {
-    if (_is_silent) return;
+    if (_is_silent)
+      return;
 
-    _progress = float(_i)/float(_i_max);
-    _pos = _progress*_barWidth;
+    _progress = float(_i) / float(_i_max);
+    _pos = _progress * _barWidth;
     std::cout << "[";
-    for (int j=0; j<_barWidth; ++j) {
-      if (j < _pos) std::cout << "=";
-      else if (j == _pos) std::cout << ">";
-      else std::cout << " ";
+    for (int j = 0; j < _barWidth; ++j)
+    {
+      if (j < _pos)
+        std::cout << "=";
+      else if (j == _pos)
+        std::cout << ">";
+      else
+        std::cout << " ";
     }
-    std::cout << "] " << int((_progress) * 100.0) << "% " << estimate_remaining_time() << "\r";
+    std::cout << "] " << int((_progress)*100.0) << "% " << estimate_remaining_time() << "\r" << std::flush;
     if (_i == _i_max)
     {
-      std::cout << "\n";
+      std::cout << std::endl;
     }
-    std::cout.flush();
     _i++;
   };
 
   // stepping function to pass progress level explicitly and do the rest internally
-  void step(const int& i)
+  void step(const int &i)
   {
-    if (_is_silent) return;
+    if (_is_silent)
+      return;
 
-    _progress = float(i)/float(_i_max);
-    _pos = _progress*_barWidth;
+    _progress = float(i) / float(_i_max);
+    _pos = _progress * _barWidth;
     std::cout << "[";
-    for (int j=0; j<_barWidth; ++j) {
-      if (j < _pos) std::cout << "=";
-      else if (j == _pos) std::cout << ">";
-      else std::cout << " ";
+    for (int j = 0; j < _barWidth; ++j)
+    {
+      if (j < _pos)
+        std::cout << "=";
+      else if (j == _pos)
+        std::cout << ">";
+      else
+        std::cout << " ";
     }
-    std::cout << "] " << int((_progress) * 100.0) << "% " << estimate_remaining_time() << "\r";
+    std::cout << "] " << int((_progress)*100.0) << "% " << estimate_remaining_time() << "\r" << std::flush;
     if (i == _i_max)
     {
-      std::cout << "\n";
+      std::cout << std::endl;
     }
-    std::cout.flush();
   };
 
   // constructor without any internal state initialization
-  progress_bar(const bool& is_silent=false)
+  progress_bar(const bool &is_silent = false)
   {
     _is_silent = is_silent;
   };
 
   // stepping function that does not need initialization
-  void step(const int& i, const int& i_max, const std::string& title)
+  void step(const int &i, const int &i_max, const std::string &title)
   {
-    if (_is_silent) return;
-    
-    _progress = float(i)/float(i_max-1);
-    _pos = _progress*_barWidth;
+    if (_is_silent)
+      return;
+
+    _progress = float(i) / float(i_max - 1);
+    _pos = _progress * _barWidth;
     std::cout << title << ": [";
-    for (int j=0; j<_barWidth; ++j) {
-      if (j < _pos) std::cout << "=";
-      else if (j == _pos) std::cout << ">";
-      else std::cout << " ";
+    for (int j = 0; j < _barWidth; ++j)
+    {
+      if (j < _pos)
+        std::cout << "=";
+      else if (j == _pos)
+        std::cout << ">";
+      else
+        std::cout << " ";
     }
-    std::cout << "] " << int((_progress) * 100.0) << "% " << estimate_remaining_time() << "\r";
-    std::cout.flush();
-    if (i == i_max-1)
+    std::cout << "] " << int((_progress)*100.0) << "% " << estimate_remaining_time() << "\r" << std::flush;
+    if (i == i_max - 1)
     {
       std::cout << std::endl;
     }
